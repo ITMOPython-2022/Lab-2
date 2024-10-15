04a475a7112c2169cc57ccdbd87577991d24ae99
Sergei Shorokhov <stratumxspb@gmail.com>
2024-10-15 17:53:29 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 from csv import reader
 
 
-flag = 0
 output = open('result.txt', 'w')
-search = input('Search for: ')
-with open('civic.csv', 'r', encoding='windows-1251') as csvfile:
-    table = reader(csvfile, delimiter=';')
-    for row in table:
-        lower_case = row[2].lower()
-        index = lower_case.find(search.lower())
-        if index != -1:
-            print(row[2])
-            flag = 1
-            output.write(f'{row[0]}. {row[2]}. Цена {row[8]} рублей.\n')
 
-    if flag == 0:
-        print('Nothing found.')
+while True:
+    flag = 0
+    search = input('Введите запрос: ')
+    if search == '0':
+        break
+    with open('civic.csv', 'r', encoding='windows-1251') as csvfile:
+        table = reader(csvfile, delimiter=';')
+        for row in table:
+            lower_case = row[2].lower()
+            index = lower_case.find(search.lower())
+            if index != -1:
+                print(row[2])
+                output.write(f'{row[2]}. Цена {row[8]} руб. S/n {row[18]}\n')
+                flag += 1
+
+
+
+        if flag == 0:
+            print('Ничего не найдено.')
+        else:
+            print(f'Найдено {flag} результатов.')
 
 output.close()
